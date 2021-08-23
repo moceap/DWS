a7962477ece5a99079a6cd76662d24be110efb14
Mosaab Alzoubi <moceap@hotmail.com>
2021-08-23 05:09:24 +0300

    Create index.php

---
+++ web/htdoc/index.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'db';
+$user = 'MYSQL_USER';
+$pass = 'MYSQL_PASSWORD';
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Server works successfully!";
+}
+?>
